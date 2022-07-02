9377477e576c19d4c0befe3464ad61d894984f8a
Jacob <jacob.juhyung.lee@gmail.com>
2022-07-02 16:22:49 +0900

    🍱 Add functions that get info from external APIs - src/api/__init__에 외부 날씨 API로 request하고 response를 받아오는 함수 작성

---
+++ src/api/__init__.py
@@ -0,0 +1,25 @@
+from httpx import AsyncClient, Response
+
+from src.config import settings
+
+
+async def get_current_info(lat: float, lon: float):
+    async with AsyncClient() as client:
+        params: dict = dict(lat=lat, lon=lon, api_key=settings.EXTERNAL_WEATHER_API_KEY)
+        resp: Response = await client.get(settings.EXTERNAL_WEATHER_CURRENT_URL, params=params)
+        results = resp.json()
+        return results
+
+
+async def get_forecast_info(lat: float, lon: float, hour_offset: int):
+    async with AsyncClient() as client:
+        params: dict = dict(lat=lat, lon=lon, hour_offset=hour_offset, api_key=settings.EXTERNAL_WEATHER_API_KEY)
+        resp: Response = await client.get(settings.EXTERNAL_WEATHER_FORECAST_URL, params=params)
+        return resp.json()
+
+
+async def get_historical_info(lat: float, lon: float, hour_offset: int):
+    async with AsyncClient() as client:
+        params: dict = dict(lat=lat, lon=lon, hour_offset=hour_offset, api_key=settings.EXTERNAL_WEATHER_API_KEY)
+        resp: Response = await client.get(settings.EXTERNAL_WEATHER_HISTORICAL_URL, params=params)
+        return resp.json()
