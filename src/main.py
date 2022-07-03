fe5cc265b2ede55b233b3e5298cbec402afe4f60
Jacob <jacob.juhyung.lee@gmail.com>
2022-07-03 23:15:02 +0900

    :bento: Add custom exception handler to override FastAPI's default 422
    - FastAPI의 디폴트 422 에러를 400에러로 치환하기 위한 작업

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
 
 from api.summary import summary_api
 
@@ -6,3 +10,34 @@ app = FastAPI()
 
 
 app.include_router(summary_api.router)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Weather Bot API",
+        version="2.5.0",
+        description="This is a OpenAPI documentation for weather bot API",
+        routes=app.routes,
+    )
+    for path in openapi_schema["paths"]:
+        for method in openapi_schema["paths"][path]:
+            if openapi_schema["paths"][path][method]["responses"].get("422"):
+                openapi_schema["paths"][path][method]["responses"][
+                    "400"
+                ] = openapi_schema["paths"][path][method]["responses"]["422"]
+                openapi_schema["paths"][path][method]["responses"].pop("422")
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
